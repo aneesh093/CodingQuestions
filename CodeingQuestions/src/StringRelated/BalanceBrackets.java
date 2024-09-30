f94e1eaa7f0a2445f4db6b9ecc550d18f7b301b8
Aneesh Mathai <aneeshmathai@Aneeshs-MacBook-Air.local>
2024-09-30 22:43:54 +0530

    Added Below Programs Balancing Brackets in String Reversing Only Vowels in a String

--- CodeingQuestions/src/StringRelated/BalanceBrackets.java
+++ CodeingQuestions/src/StringRelated/BalanceBrackets.java
@@ -6,29 +6,27 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class BalanceBrackets {
-
 	public static void main(String[] args) {
+		String str = "([{}])";
+		Map<String, Long> testMap = Stream.of(str.split(""))
+				.collect(Collectors.groupingBy(Function.identity(), 
+						Collectors.counting()));
 
-		String str ="([{}])"; 
-		Map<String, Long> testMap=Stream.of(str.split(""))
-					.collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
-			int count1=0;
-			
-			if(testMap.get("(") == testMap.get(")")) {
-				count1++;
-			}
-			if(testMap.get("{") == testMap.get("}")) {
-				count1++;
-			}
-			if(testMap.get("[") == testMap.get("]")) {
-				count1++;
-			}
-			
-		
-		if(count1==3)
-			System.out.println("Matching");
+		int count = 0;
+		if (testMap.get("(") == testMap.get(")")) {
+			count++;
+		}
+		if (testMap.get("{") == testMap.get("}")) {
+			count++;
+		}
+		if (testMap.get("[") == testMap.get("]")) {
+			count++;
+		}
+
+		if (count == (testMap.size()/2))
+			System.out.println("Balanced String");
 		else
-			System.out.println("Not Matching");
+			System.out.println("Not a Balanced String");
 	}
 
 }
